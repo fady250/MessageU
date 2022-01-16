32adb87ff3b75013642b13ce9854c27774e0084b
fady250 <fady.abu.roken@intel.com>
2022-01-16 22:11:28 +0200

    add the guid import

--- MessageUServer/Server.py
+++ MessageUServer/Server.py
@@ -1,5 +1,5 @@
 import socket
-
+import uuid
 
 class Server(object):
     def __init__(self):
@@ -9,18 +9,20 @@ class Server(object):
                 self.__HOST = ''
                 self.__clients = []             # holds objects of type ClientEntry
                 # disctionary that holds client as key and list of messages as value
+                self.handle_client_request()
         except FileNotFoundError:
             print("File myport.info not found !")
             exit()
 
     def start(self):
+
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self.__HOST, self.__PORT))
             s.listen()
             conn, addr = s.accept()
             with conn:
                 print('Connected by', addr)
-                # check in the examples madrich if we have examopkle how to handle 
+                # check in the examples madrich if we have examopkle how to handle -- selector
                 while True:
                     data = conn.recv(1024)
                     text = data.decode("utf-8")
@@ -32,3 +34,7 @@ class Server(object):
                     for i in range(min(len(replydata), len(newdata))):
                         newdata[i] = replydata[i]
                     conn.sendall(newdata)
+
+    def handle_client_request(self):
+        uid = uuid.uuid4()
+        print(uid)   # TODO do we need to remove the dash ?
