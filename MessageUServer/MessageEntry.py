206126ca66e42c51ee391f13d28e35c0ae7ce5bb
fady250 <fady.abu.roken@intel.com>
2022-02-28 21:30:04 +0200

    move server to db + some fixes in the client + delete unneeded files in the server after moving to db

--- MessageUServer/MessageEntry.py
+++
@@ -1,7 +0,0 @@
-class MessageEntry(object):
-    def __init__(self, id, to_id, from_id, type, content):
-        self.ID = id
-        self.ToClient = to_id
-        self.FromClient = from_id
-        self.Type = type
-        self.Content = content
